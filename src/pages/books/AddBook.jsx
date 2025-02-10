bc288997569226dae8fddc7f991b5a032d98e63a
piyush.gyl <piyush2022ug@gmail.com>
2025-02-10 12:38:54 +0530

    feat: add-member page built

--- src/pages/books/AddBook.jsx
+++ src/pages/books/AddBook.jsx
@@ -56,152 +56,148 @@ export default function AddBook() {
   }, [addStatus, setFormData]);
 
   return (
-    <>
-      <main className="container my-5">
-        <h1> Add Book </h1>
-        {showSuccess && (
-          <div
-            className="alert alert-success alert-dismissible fade show"
-            role="alert"
+    <main className="container my-5">
+      <h1> Add Book </h1>
+      {showSuccess && (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          Book has been successfully added!
+          <button
+            type="button"
+            className="btn-close"
+            onClick={() => setShowSuccess(false)}
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
+      {addStatus === "error" && (
+        <div className="alert alert-danger">Error adding book</div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="author" className="form-label">
+            Author:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="author"
+            value={formData.author}
+            onChange={(e) =>
+              setFormData({ ...formData, author: e.target.value })
+            }
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="isbn" className="form-label">
+            ISBN:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="isbn"
+            value={formData.isbn}
+            onChange={(e) => setFormData({ ...formData, isbn: e.target.value })}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="genre" className="form-label">
+            Genre:
+          </label>
+          <select
+            className="form-select"
+            id="genre"
+            value={formData.genre}
+            onChange={(e) =>
+              setFormData({ ...formData, genre: e.target.value })
+            }
+            required
           >
-            Book has been successfully added!
-            <button
-              type="button"
-              className="btn-close"
-              onClick={() => setShowSuccess(false)}
-              aria-label="Close"
-            ></button>
-          </div>
-        )}
-        {addStatus === "error" && (
-          <div className="alert alert-danger">Error adding book</div>
-        )}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Title:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              value={formData.title}
-              onChange={(e) =>
-                setFormData({ ...formData, title: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="author" className="form-label">
-              Author:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="author"
-              value={formData.author}
-              onChange={(e) =>
-                setFormData({ ...formData, author: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="isbn" className="form-label">
-              ISBN:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="isbn"
-              value={formData.isbn}
-              onChange={(e) =>
-                setFormData({ ...formData, isbn: e.target.value })
-              }
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="genre" className="form-label">
-              Genre:
-            </label>
-            <select
-              className="form-select"
-              id="genre"
-              value={formData.genre}
-              onChange={(e) =>
-                setFormData({ ...formData, genre: e.target.value })
-              }
-              required
-            >
-              <option value="">Select Genre</option>
-              <option value="Fiction">Fiction</option>
-              <option value="Non-Fiction">Non-Fiction</option>
-              <option value="Technology">Technology</option>
-              <option value="History">History</option>
-              <option value="Literature">Literature</option>
-            </select>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="publicationYear" className="form-label">
-              Publication Year:
-            </label>
-            <input
-              type="number"
-              className="form-control"
-              id="publicationYear"
-              value={formData.publicationYear}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  publicationYear: parseFloat(e.target.value),
-                })
-              }
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="totalCopies" className="form-label">
-              Total Copies:
-            </label>
-            <input
-              type="number"
-              className="form-control"
-              id="totalCopies"
-              value={formData.totalCopies}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  totalCopies: parseFloat(e.target.value),
-                })
-              }
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="availableCopies" className="form-label">
-              Available Copies:
-            </label>
-            <input
-              type="number"
-              className="form-control"
-              id="availableCopies"
-              value={formData.availableCopies}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  availableCopies: parseFloat(e.target.value),
-                })
-              }
-              required
-            />
-          </div>
-          <button className="btn btn-primary" type="submit">
-            {addStatus === "loading" ? "Adding" : "Add Book"}
-          </button>
-        </form>
-      </main>
-    </>
+            <option value="">Select Genre</option>
+            <option value="Fiction">Fiction</option>
+            <option value="Non-Fiction">Non-Fiction</option>
+            <option value="Technology">Technology</option>
+            <option value="History">History</option>
+            <option value="Literature">Literature</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="publicationYear" className="form-label">
+            Publication Year:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="publicationYear"
+            value={formData.publicationYear}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                publicationYear: parseFloat(e.target.value),
+              })
+            }
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="totalCopies" className="form-label">
+            Total Copies:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="totalCopies"
+            value={formData.totalCopies}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                totalCopies: parseFloat(e.target.value),
+              })
+            }
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="availableCopies" className="form-label">
+            Available Copies:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="availableCopies"
+            value={formData.availableCopies}
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                availableCopies: parseFloat(e.target.value),
+              })
+            }
+            required
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          {addStatus === "loading" ? "Adding" : "Add Book"}
+        </button>
+      </form>
+    </main>
   );
 }
--- src/pages/members/AddMember.jsx
+++ src/pages/members/AddMember.jsx
@@ -1,10 +1,129 @@
-
-
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  getMemberStatus,
+  postMember,
+} from "../../features/members/memberSlice";
 
 export default function AddMember() {
-    return (
-        <>
-            <h1> Add Member </h1>
-        </> 
-    )
+  //SETTING INITIAL FORM STATE
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    membershipType: "",
+  });
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  //useNavigate
+  const navigate = useNavigate();
+
+  //useDispatch
+  const dispatch = useDispatch();
+
+  //get add status state from the store
+  const { addStatus } = useSelector(getMemberStatus);
+
+  //handleSubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      dispatch(postMember(formData));
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+        navigate("/members");
+      }, 3000);
+    } catch (error) {
+      console.log("POST ERROR", error);
+    }
+  };
+
+  useEffect(() => {
+    if (addStatus === "success") {
+      setFormData({ name: "", email: "", membershipType: "" });
+    }
+  }, [addStatus, setFormData]);
+
+  return (
+    <main className="container my-5">
+      <h1>Add Member</h1>
+      {showSuccess && (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          Member has been successfully added!
+          <button
+            type="button"
+            className="btn-close"
+            onClick={() => setShowSuccess(false)}
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
+      {addStatus === "error" && (
+        <div className="alert alert-danger">Error adding book</div>
+      )}
+      <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="membershipType" className="form-label">
+                  Membership Type:
+                </label>
+                <select
+                  className="form-select"
+                  id="membershipType"
+                  value={formData.membershipType}
+                  onChange={(e) =>
+                    setFormData({ ...formData, membershipType: e.target.value })
+                  }
+                  required
+                >
+                  <option value="">Select Membership Type</option>
+                  <option value="Standard">Standard</option>
+                  <option value="Premium">Premium</option>
+                  <option value="Student">Student</option>
+                </select>
+              </div>
+            <button
+              className="btn btn-primary"
+              type="submit"
+              disabled={addStatus === "loading"}
+            >
+              {addStatus === "loading" ? "Adding" : "Add Member"}
+            </button>
+          </form>
+    </main>
+  );
 }
